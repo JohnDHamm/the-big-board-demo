b1d211fcbd4afd9245cc6610fda9f5bc2b067380
John Hamm <johndhamm1@gmail.com>
2020-07-15 13:48:41 -0500

    feat(style): add styled-components, TestComponent

---
+++ src/components/TestComponent/TestComponent.styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: #bada55;
+  border: 1px solid navy;
+  padding: 1rem 3rem;
+  border-radius: 0.5rem;
+  color: white;
+`;

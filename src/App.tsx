a4eef4b37335084361652db30eae5cc805814fe9
John Hamm <johndhamm1@gmail.com>
2020-07-15 15:05:59 -0500

    feat(app): add ROUTES constant

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ROUTES } from './routes';
 import {
   BoardPage,
   HomePage,
@@ -7,16 +8,18 @@ import {
   MyTeamPage,
   PlayersPage,
 } from './pages';
+import { NavBar } from './components';
 
 function App() {
   return (
     <Router>
+      <NavBar />
       <Switch>
-        <Route path="/board" component={BoardPage} />
-        <Route path="/players" component={PlayersPage} />
-        <Route path="/my-team" component={MyTeamPage} />
-        <Route path="/more" component={MorePage} />
-        <Route path="/" component={HomePage} />
+        <Route path={ROUTES.BOARD} component={BoardPage} />
+        <Route path={ROUTES.PLAYERS} component={PlayersPage} />
+        <Route path={ROUTES.MY_TEAM} component={MyTeamPage} />
+        <Route path={ROUTES.MORE} component={MorePage} />
+        <Route path={ROUTES.HOME} component={HomePage} />
       </Switch>
     </Router>
   );
---
+++ src/routes/index.ts
@@ -0,0 +1,7 @@
+export const ROUTES = {
+  HOME: '/',
+  BOARD: '/board',
+  PLAYERS: '/players',
+  MY_TEAM: '/my-team',
+  MORE: '/more',
+};

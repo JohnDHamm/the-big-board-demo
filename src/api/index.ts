8862764b4c1dc350db654c71b4cfa104d93ba146
John Hamm <johndhamm1@gmail.com>
2020-07-18 17:10:59 -0500

    feat(api): add getLeague + getOwners

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,8 @@
+import { getLeague } from './getLeague/getLeague';
 import { getLeaguesList } from './getLeaguesList/getLeaguesList';
+import { getOwners } from './getOwners/getOwners';
 import { login } from './login/login';
 
 export const DEV_API_ROOT_URL = 'http://localhost:4001';
 
-export { getLeaguesList, login };
+export { getLeague, getLeaguesList, getOwners, login };
--- src/pages/AppLoadingPage/AppLoadingPage.tsx
+++ src/pages/AppLoadingPage/AppLoadingPage.tsx
@@ -8,6 +8,7 @@ import {
   MyTeamContext,
   DraftContext,
 } from '../../contexts';
+import { getLeague, getOwners } from '../../api';
 
 const AppLoadingPage: React.FC = () => {
   const { draft } = React.useContext(DraftContext);
@@ -16,17 +17,44 @@ const AppLoadingPage: React.FC = () => {
   const { teams } = React.useContext(TeamsContext);
   const { user } = React.useContext(UserContext);
 
+  const [league, setLeague] = React.useState<League | null>(null);
+  const [owners, setOwners] = React.useState<Owner[] | null>(null);
+
+  const initDraft = (leagueId: string) => {
+    getLeague(leagueId)
+      .then((usersLeague: League) => {
+        if (usersLeague) {
+          setLeague(usersLeague);
+        }
+      })
+      .then(() =>
+        getOwners(leagueId).then((leagueOwners: Owner[]) => {
+          if (leagueOwners.length > 0) {
+            setOwners(leagueOwners);
+          }
+        })
+      );
+  };
+
   React.useEffect(() => {
-    console.log('draft', draft);
-    console.log('players', players);
-    console.log('teams', teams);
-    console.log('user', user);
-    console.log('myTeam', myTeam);
-  }, [draft, myTeam, players, teams, user]);
+    // console.log('players', players);
+    // console.log('teams', teams);
+    // console.log('user', user);
+    // console.log('league', league);
+    console.log('owners', owners);
+  }, [league, owners]);
+
+  React.useEffect(() => {
+    if (user) {
+      initDraft(user.leagueId);
+    }
+  }, [user]);
 
   return (
     <div>
-      <h1>loading data...</h1>
+      <h1>preparing draft data for</h1>
+      {league && <h3>{league.name}</h3>}
+
       <Link to={ROUTES.BOARD}>
         <button>data is loaded</button>
       </Link>

1e51ef41b0b1848fb8eccf160e3cd1c943acc243
John Hamm <johndhamm1@gmail.com>
2020-07-20 23:44:06 -0500

    feat(AppLoadingPage): create draft picks and update draft context

--- src/hooks/useDraft/useDraft.ts
+++ src/hooks/useDraft/useDraft.ts
@@ -1,8 +1,8 @@
 import React from 'react';
-import { mockDraft } from '../../contexts/DraftContext/DraftContext';
+import { DRAFT_INITIAL_VALUE } from '../../contexts/DraftContext/DraftContext';
 
 export const useDraft = (): DraftContextInterface => {
-  const [draft, setDraft] = React.useState<Draft>(mockDraft);
+  const [draft, setDraft] = React.useState<Draft>(DRAFT_INITIAL_VALUE);
 
   const setCurrentDraft = React.useCallback((currentDraft: Draft): void => {
     setDraft(currentDraft);
--- src/types/DraftPick.d.ts
+++ src/types/DraftPick.d.ts
@@ -1,8 +1,13 @@
-interface DraftPick {
+interface CurrentDraftPick {
   selectionNumber: number;
   ownerId: string;
+}
+
+interface PlayerId {
   playerId: string;
 }
+
+type DraftPick = CurrentDraftPick & PlayerId;
 
 interface DraftPickContext {
   [key: number]: DraftPick;

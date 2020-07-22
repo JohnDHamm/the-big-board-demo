ebf913569548fb1b933c31c5ef920f7007f16215
John Hamm <johndhamm1@gmail.com>
2020-07-21 19:57:04 -0500

    feat(BoardPage): add render of picks

--- src/pages/BoardPage/BoardPage.tsx
+++ src/pages/BoardPage/BoardPage.tsx
@@ -1,18 +1,62 @@
 import React from 'react';
-import { DraftContext, PlayersContext } from '../../contexts';
+import { PickCard } from '../../components';
+import { DraftContext, PlayersContext, TeamsContext } from '../../contexts';
+import { calcPickRoundNumber } from '../../functions';
 
 const BoardPage: React.FC = () => {
   const { draft } = React.useContext(DraftContext);
   const { players } = React.useContext(PlayersContext);
+  const { teams } = React.useContext(TeamsContext);
+
+  const [draftStarted, setDraftStarted] = React.useState<boolean>(false);
+  const picksPerRound = draft.owners.length;
+  const [currentRoundNum, setCurrentRoundNum] = React.useState<number>();
+
+  const renderPicks = (roundNum: number): JSX.Element[] => {
+    const { picks } = draft;
+    const listEndNum = roundNum * picksPerRound;
+    const listStartNum = listEndNum - picksPerRound + 1;
+    const foo: JSX.Element[] = [];
+    for (let i = listStartNum; i < listEndNum + 1; i++) {
+      const owner = draft.owners.find((owner) => picks[i].ownerId === owner.id);
+      const player = players[picks[i].playerId];
+      foo.push(
+        <PickCard
+          key={i}
+          selectionNumber={picks[i].selectionNumber}
+          ownerName={owner?.name || ''}
+          player={player}
+          team={player && player.teamId ? teams[player.teamId] : null}
+        />
+      );
+    }
+    return foo;
+  };
 
   React.useEffect(() => {
-    console.log('draft', draft);
-    console.log('players', players);
+    setCurrentRoundNum(
+      calcPickRoundNumber(draft.currentPick.selectionNumber, picksPerRound)
+    );
+  }, [picksPerRound, draft]);
+
+  React.useEffect(() => {
+    // console.log('draft', draft);
+    // console.log('players', players);
+    if (draft.league.draftStatus !== 'not started') {
+      setDraftStarted(true);
+    }
   }, [draft, players]);
 
   return (
     <div>
-      <h1>The Big Board</h1>
+      {draftStarted ? (
+        <div>
+          <h3>round: {currentRoundNum}</h3>
+          {currentRoundNum && renderPicks(currentRoundNum)}
+        </div>
+      ) : (
+        <h3>DRAFT HAS NOT YET STARTED</h3>
+      )}
     </div>
   );
 };

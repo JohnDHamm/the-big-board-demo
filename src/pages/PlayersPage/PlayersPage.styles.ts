90731c74745747a866cb604fa7b496ad6496e6cf
John Hamm <johndhamm1@gmail.com>
2020-07-22 18:28:09 -0500

    feat(PlayersPage): display player cards

---
+++ src/pages/PlayersPage/PlayersPage.styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const PageContainer = styled.div`
+  padding: 1rem;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+`;
+
+export const CenterContent = styled.div`
+  width: 100%;
+  max-width: 400px;
+`;

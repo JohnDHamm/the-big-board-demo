677516fa421adbcf1da2a4a0841a8f44dcae3edb
John Hamm <johndhamm1@gmail.com>
2020-07-23 14:19:37 -0500

    feat(styles): add COLORS for positions, primary colors

---
+++ src/styles/colors/colors.ts
@@ -0,0 +1,16 @@
+const NFL_POSITIONS = {
+  QB: '#be1e2d',
+  RB: '#2e3191',
+  WR: '#552d90',
+  TE: '#f06b00',
+  D: '#009345',
+  K: '#5a4a42',
+};
+
+export const COLORS = {
+  BLACK: '#000000',
+  NFL_POSITIONS,
+  PRIMARY_GREEN: '#8bc53f',
+  DISABLED_GRAY: '#CECECE',
+  WHITE: '#FFFFFF',
+};

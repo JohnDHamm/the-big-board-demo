5e7b1f28c5535368e9dc0966f9eaca268e7ff97c
John Hamm <johndhamm1@gmail.com>
2020-07-27 14:04:32 -0500

    feat(MyTeamPage): display position blocks

---
+++ src/pages/MyTeamPage/MyTeamPage.styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Page = styled.div`
+  padding-top: 2rem;
+  display: flex;
+  flex: 1;
+  flex-wrap: wrap;
+  justify-content: space-around;
+`;
--- src/pages/MyTeamPage/MyTeamPage.tsx
+++ src/pages/MyTeamPage/MyTeamPage.tsx
@@ -1,11 +1,56 @@
 import React from 'react';
+import { Page } from './MyTeamPage.styles';
+import { MyTeamPositionBlock } from '../../components';
+import {
+  MyTeamContext,
+  DraftContext,
+  TeamsContext,
+  PlayersContext,
+} from '../../contexts';
+import { calcPickRoundNumber } from '../../functions';
+
+const positions: NFL_Position[] = ['QB', 'RB', 'WR', 'TE', 'D', 'K'];
 
 const MyTeamPage: React.FC = () => {
-  return (
-    <div>
-      <h1>My Team</h1>
-    </div>
-  );
+  const { myTeam } = React.useContext(MyTeamContext);
+  const { draft } = React.useContext(DraftContext);
+  const { teams } = React.useContext(TeamsContext);
+  const { players } = React.useContext(PlayersContext);
+
+  const renderBlocks = (): JSX.Element[] => {
+    let blocks: JSX.Element[] = [];
+    positions.forEach((position) => {
+      const positionSlots = draft.league.positionSlots.filter(
+        (slot) => slot.position === position
+      );
+      const totalSlots = positionSlots[0].total;
+      let myPlayers: MyPlayer[] = [];
+      myTeam.forEach((player) => {
+        if (players[player.playerId].position === position) {
+          const newPlayer: MyPlayer = {
+            playerInfo: players[player.playerId],
+            roundSelectionNum: calcPickRoundNumber(
+              player.selectionNumber,
+              draft.owners.length
+            ),
+            team: teams[players[player.playerId].teamId],
+          };
+          myPlayers.push(newPlayer);
+        }
+      });
+      blocks.push(
+        <MyTeamPositionBlock
+          key={position}
+          position={position}
+          totalSlots={totalSlots}
+          players={myPlayers}
+        />
+      );
+    });
+    return blocks;
+  };
+
+  return <Page>{renderBlocks()}</Page>;
 };
 
 export default MyTeamPage;

3f4c11d2157529716f2f638a00623e67cb3150a7
John Hamm <johndhamm1@gmail.com>
2020-08-09 14:12:29 -0500

    feat(contexts, hooks): add PickConfirmModalContext and usePickConfirmModal hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,7 +1,15 @@
 import { useDraft } from './useDraft/useDraft';
 import { useMyTeam } from './useMyTeam/useMyTeam';
+import { usePickConfirmModal } from './usePickConfirmModal/usePickConfirmModal';
 import { usePlayers } from './usePlayers/usePlayers';
 import { useTeams } from './useTeams/useTeams';
 import { useUser } from './useUser/useUser';
 
-export { useDraft, useMyTeam, usePlayers, useTeams, useUser };
+export {
+  useDraft,
+  useMyTeam,
+  usePickConfirmModal,
+  usePlayers,
+  useTeams,
+  useUser,
+};

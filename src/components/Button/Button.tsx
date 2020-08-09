313e96d2075e24707a652dc8e9206ad7ae59ddfa
John Hamm <johndhamm1@gmail.com>
2020-08-09 14:09:30 -0500

    refactor(Button): add alternate prop for styling

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,12 +4,13 @@ import { Container, Label } from './Button.styles';
 interface Props {
   label: string;
   width?: string;
+  alternate?: boolean;
 }
 
-const Button: React.FC<Props> = ({ label, width }) => {
+const Button: React.FC<Props> = ({ label, width, alternate = false }) => {
   return (
-    <Container width={width}>
-      <Label>{label}</Label>
+    <Container width={width} alternate={alternate}>
+      <Label alternate={alternate}>{label}</Label>
     </Container>
   );
 };
